87386178a6bafe1d19b54bb86fce3108815c9012
Francesco <francesco@monzo.com>
2022-02-08 16:07:21 +0100

    first commit

---
+++ app.rb
@@ -0,0 +1,13 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class Battle < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  # our routes would go here
+
+  # # Start the server if this file is executed directly (do not change the line below)
+  run! if app_file == $0
+end
